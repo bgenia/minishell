10de8da960bf6571922ce1a9575fcfd143e58b12
bgenia <bgenia@student.21-school.ru>
2021-11-09 20:45:03 +0300

    Implement parser_peek

---
+++ src/parser/utils/parser_peek.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser_peek.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bgenia <bgenia@student.21-school.ru>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/09 20:25:16 by bgenia            #+#    #+#             */
+/*   Updated: 2021/11/09 20:30:16 by bgenia           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include <minishell/parser/parser.h>
+
+t_token	parser_peek(t_parser *parser, int offset)
+{
+	if (parser->position + offset < parser->length)
+		return (parser->vec_tokens[parser->position + offset]);
+	return ((t_token){.type = TOKEN_EOF});
+}
