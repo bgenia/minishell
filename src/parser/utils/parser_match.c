b9f959eeca51dfc1d41ab62e87ad447f079b5c67
bgenia <bgenia@student.21-school.ru>
2021-11-09 20:45:28 +0300

    Implement parser_match

---
+++ src/parser/utils/parser_match.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser_match.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bgenia <bgenia@student.21-school.ru>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/09 20:32:49 by bgenia            #+#    #+#             */
+/*   Updated: 2021/11/09 20:34:39 by bgenia           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <minishell/parser/parser.h>
+#include <minishell/lexer/token.h>
+
+bool	parser_match(t_parser *parser, t_token_type type)
+{
+	if (parser_peek(parser, 0).type == type)
+	{
+		parser_skip(parser, 1);
+		return (true);
+	}
+	return (false);
+}
