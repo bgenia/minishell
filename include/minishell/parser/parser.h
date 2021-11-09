0ac9e035f4acb38f92af7ea096d8893b0dbe1a2c
bgenia <bgenia@student.21-school.ru>
2021-11-09 20:49:04 +0300

    Update parser.h

--- include/minishell/parser/parser.h
+++ include/minishell/parser/parser.h
@@ -6,7 +6,7 @@
 /*   By: bgenia <bgenia@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/09 19:50:21 by bgenia            #+#    #+#             */
-/*   Updated: 2021/11/09 20:16:41 by bgenia           ###   ########.fr       */
+/*   Updated: 2021/11/09 20:48:46 by bgenia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,22 +19,23 @@
 
 typedef enum e_parser_status
 {
-	PARSER_ERROR = -1,
-	PARSER_OK
+	PARSER_OK,
+	PARSER_ERROR_BAD_SYNTAX
 }	t_parser_status;
 
-typedef enum e_parser_error
+typedef struct s_parser_error_info
 {
-    
-}	t_parser_error;
+	t_token_type	expected;
+	t_token			got;
+}	t_parser_error_info;
 
 typedef struct s_parser
 {
-    t_token         *vec_tokens;
-	size_t			position;
-	size_t			length;
-	t_parser_status	status;
-	t_parser_error	error;
+    t_token         	*vec_tokens;
+	size_t				position;
+	size_t				length;
+	t_parser_status		status;
+	t_parser_error_info	error_info;
 }	t_parser;
 
 t_parser
@@ -57,17 +58,17 @@ char
 
 
 t_parser_status
-parser_raise_error(t_parser *parser, t_parser_error error);
+parser_set_status(t_parser *parser, t_parser_status status);
 
-char
+t_token
 parser_peek(t_parser *parser, int offset);
-char
+t_token
 parser_skip(t_parser *parser, int count);
 bool
 parser_is_end(t_parser *parser);
 bool
 parser_match(t_parser *parser, t_token_type type);
-t_token
-*parser_consume(t_parser *parser, t_token_type type);
+bool
+parser_consume(t_parser *parser, t_token_type type);
 
 #endif
