b3481b2f096dd9c4c838a2ab709b8e3e2ce7f661
bgenia <bgenia@student.21-school.ru>
2021-11-10 22:43:11 +0300

    Fix memleak in remove_whitespaces

--- src/preparser/remove_whitespaces.c
+++ src/preparser/remove_whitespaces.c
@@ -6,9 +6,11 @@
 /*   By: bgenia <bgenia@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/31 19:16:22 by bgenia            #+#    #+#             */
-/*   Updated: 2021/11/01 12:07:55 by bgenia           ###   ########.fr       */
+/*   Updated: 2021/11/10 22:26:52 by bgenia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdlib.h>
 
 #include <libft/vector/vector.h>
 
@@ -19,10 +21,13 @@ void	remove_whitespaces(t_token **token_vec_ptr)
 	size_t	i;
 
 	i = 0;
-	while (i < ft_vector_get_size(token_vec_ptr))
+	while (i < ft_vector_get_size(*token_vec_ptr))
 	{
 		if ((*token_vec_ptr)[i].type == TOKEN_SPACE)
+		{
+			free((*token_vec_ptr)[i].value);
 			ft_vector_remove_at(token_vec_ptr, i);
+		}
 		else
 			i++;
 	}
