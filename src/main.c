424503549df25282a827ebc120f758f2d73f4b66
bgenia <bgenia@student.21-school.ru>
2021-11-10 22:40:38 +0300

    Test main

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: bgenia <bgenia@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/22 17:19:46 by bgenia            #+#    #+#             */
-/*   Updated: 2021/11/10 21:37:42 by bgenia           ###   ########.fr       */
+/*   Updated: 2021/11/10 22:33:57 by bgenia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,17 @@
 
 #include <libft/vector/vector.h>
 
+#include <libft/system/env.h>
+
 #include <minishell/lexer/lexer.h>
 #include <minishell/lexer/token.h>
+
+#include <minishell/parser/ast.h>
+#include <minishell/parser/parser.h>
+
+#include <minishell/expander/expander.h>
+
+#include <minishell/preparser/preparser.h>
 
 int	main(int argc, char **argv)
 {
@@ -26,12 +35,22 @@ int	main(int argc, char **argv)
 	size_t	i;
 
 	(void)argc;
+	ft_printf("ARGV>> %s\n", argv[1]);
 	vec_tokens = ft_vector_alloc_empty(sizeof(*vec_tokens));
 	lexer = lexer_create(&vec_tokens);
 	if (lexer_analyze(&lexer, argv[1]) != LEXER_OK)
 		ft_dprintf(STDERR_FILENO, "Lexer error!\n");
+	lexer_destroy(&lexer);
+	for (size_t i = 0; i < ft_vector_get_size(vec_tokens); i++)
+		if (vec_tokens[i].is_expandable)
+			expand_env_variables(&vec_tokens[i].value);
+	split_simple_words(&vec_tokens);
+	merge_adjacent_words(&vec_tokens);
+	remove_whitespaces(&vec_tokens);
 	ft_printf("Token list [%u]:\n\n", ft_vector_get_size(vec_tokens));
 	i = 0;
+	while (i < ft_vector_get_size(vec_tokens))
+	{
 		ft_printf(
 			":%d %d \"%s\"[%d] { fd: %d, expandable: %d }\n",
 			vec_tokens[i].position,
@@ -41,10 +60,32 @@ int	main(int argc, char **argv)
 			vec_tokens[i].fd,
 			vec_tokens[i].is_expandable
 			);
-		free(vec_tokens[i].value);
 		i++;
 	}
 	ft_printf("\nEnd.\n");
-	lexer_destroy(&lexer);
+	t_ast	ast;
+
+	t_parser parser = parser_create(&ast);
+	if (parser_parse(&parser, vec_tokens) != PARSER_OK)
+	{
+		ft_printf("Parser error! at %d\n", parser.error_info.got.position);
+		ft_printf("> Expected: [%d]\n", parser.error_info.expected);
+		ft_printf("> Got: [%d] %s\n", parser.error_info.got.type, parser.error_info.got.value);
+	}
+	parser_destroy(&parser);
+	for (size_t i = 0; i < ft_vector_get_size(vec_tokens); i++)
+		free(vec_tokens[i].value);
 	ft_vector_free(vec_tokens);
+	ft_printf("> %p\n", &ast);
+	for (size_t i = 0; i < ft_vector_get_size(ast.pipeline.vec_commands); i++)
+	{
+		for (size_t j = 0; j < ft_vector_get_size(ast.pipeline.vec_commands[i].vec_argv); j++)
+			free(ast.pipeline.vec_commands[i].vec_argv[j]);
+		for (size_t j = 0; j < ft_vector_get_size(ast.pipeline.vec_commands[i].vec_redirections); j++)
+			free(ast.pipeline.vec_commands[i].vec_redirections[j].source);
+		ft_vector_free(ast.pipeline.vec_commands[i].vec_argv);
+		ft_vector_free(ast.pipeline.vec_commands[i].vec_redirections);
+	}
+	ft_vector_free(ast.pipeline.vec_commands);
+	ft_clearenv();
 }
