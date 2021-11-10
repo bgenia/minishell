fd6230d62b1be90ed268bd423ef4d71f7857bd39
bgenia <bgenia@student.21-school.ru>
2021-11-10 21:38:02 +0300

    ss

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: bgenia <bgenia@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/22 17:19:46 by bgenia            #+#    #+#             */
-/*   Updated: 2021/11/10 21:31:32 by bgenia           ###   ########.fr       */
+/*   Updated: 2021/11/10 21:37:42 by bgenia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,8 +32,6 @@ int	main(int argc, char **argv)
 		ft_dprintf(STDERR_FILENO, "Lexer error!\n");
 	ft_printf("Token list [%u]:\n\n", ft_vector_get_size(vec_tokens));
 	i = 0;
-	while (i < ft_vector_get_size(vec_tokens))
-	{
 		ft_printf(
 			":%d %d \"%s\"[%d] { fd: %d, expandable: %d }\n",
 			vec_tokens[i].position,
