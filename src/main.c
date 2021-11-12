87a9fa1d21bfb0a03b719e472cf108f930685480
bgenia <bgenia@student.21-school.ru>
2021-11-12 23:03:38 +0300

    main

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: bgenia <bgenia@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/22 17:19:46 by bgenia            #+#    #+#             */
-/*   Updated: 2021/11/10 22:33:57 by bgenia           ###   ########.fr       */
+/*   Updated: 2021/11/12 22:57:35 by bgenia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@
 
 #include <minishell/parser/ast.h>
 #include <minishell/parser/parser.h>
+#include <minishell/parser/parser_utils.h>
 
 #include <minishell/expander/expander.h>
 
@@ -62,21 +63,33 @@ int	main(int argc, char **argv)
 			);
 		i++;
 	}
-	ft_printf("\nEnd.\n");
-	t_ast	ast;
-
+	ft_printf("\nEnd.\n\n");
+	t_ast ast;
 	t_parser parser = parser_create(&ast);
 	if (parser_parse(&parser, vec_tokens) != PARSER_OK)
 	{
-		ft_printf("Parser error! at %d\n", parser.error_info.got.position);
-		ft_printf("> Expected: [%d]\n", parser.error_info.expected);
-		ft_printf("> Got: [%d] %s\n", parser.error_info.got.type, parser.error_info.got.value);
+		ft_dprintf(STDERR_FILENO, "%S\n", create_parser_error_message(&parser.error_info));
+	}
+	else
+	{
+		ft_printf("Pipeline:\n");
+		for (size_t i = 0; i < ft_vector_get_size(ast.pipeline.vec_commands); i++)
+		{
+			t_ast_command command = ast.pipeline.vec_commands[i];
+			ft_printf("\tCommand:");
+			for (size_t i = 0; i < ft_vector_get_size(command.vec_argv); i++)
+				ft_printf(" %s", command.vec_argv[i]);
+			ft_printf("\n");
+			for (size_t i = 0; i < ft_vector_get_size(command.vec_redirections); i++)
+			{
+				ft_printf("\t\tRedirection: fd=%d source='%s'\n", command.vec_redirections[i].fd, command.vec_redirections[i].source);
+			}
+		}
 	}
 	parser_destroy(&parser);
 	for (size_t i = 0; i < ft_vector_get_size(vec_tokens); i++)
 		free(vec_tokens[i].value);
 	ft_vector_free(vec_tokens);
-	ft_printf("> %p\n", &ast);
 	for (size_t i = 0; i < ft_vector_get_size(ast.pipeline.vec_commands); i++)
 	{
 		for (size_t j = 0; j < ft_vector_get_size(ast.pipeline.vec_commands[i].vec_argv); j++)
