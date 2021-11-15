0be4568066a306cfa2478641c245c06e3a1ea912
bgenia <bgenia@student.21-school.ru>
2021-11-15 08:53:17 +0300

    Update main

--- src/main.c
+++ src/main.c
@@ -6,99 +6,86 @@
 /*   By: bgenia <bgenia@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/22 17:19:46 by bgenia            #+#    #+#             */
-/*   Updated: 2021/11/12 22:57:35 by bgenia           ###   ########.fr       */
+/*   Updated: 2021/11/14 19:49:34 by bgenia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include <stdbool.h>
 
+#include <sys/wait.h>
+
+#include <libft/string/string.h>
 #include <libft/io/printf.h>
-
 #include <libft/vector/vector.h>
-
 #include <libft/system/env.h>
 
-#include <minishell/lexer/lexer.h>
-#include <minishell/lexer/token.h>
-
-#include <minishell/parser/ast.h>
-#include <minishell/parser/parser.h>
-#include <minishell/parser/parser_utils.h>
-
+#include <minishell/repl/repl.h>
+#include <minishell/repl/signals.h>
 #include <minishell/expander/expander.h>
+#include <minishell/builtins.h>
 
-#include <minishell/preparser/preparser.h>
+#include <stdio.h>
+#include <readline/readline.h>
+#include <readline/history.h>
 
 int	main(int argc, char **argv)
 {
-	t_token	*vec_tokens;
-	t_lexer	lexer;
-	size_t	i;
+	char	*line;
+	t_ast	ast;
 
-	(void)argc;
-	ft_printf("ARGV>> %s\n", argv[1]);
-	vec_tokens = ft_vector_alloc_empty(sizeof(*vec_tokens));
-	lexer = lexer_create(&vec_tokens);
-	if (lexer_analyze(&lexer, argv[1]) != LEXER_OK)
-		ft_dprintf(STDERR_FILENO, "Lexer error!\n");
-	lexer_destroy(&lexer);
-	for (size_t i = 0; i < ft_vector_get_size(vec_tokens); i++)
-		if (vec_tokens[i].is_expandable)
-			expand_env_variables(&vec_tokens[i].value);
-	split_simple_words(&vec_tokens);
-	merge_adjacent_words(&vec_tokens);
-	remove_whitespaces(&vec_tokens);
-	ft_printf("Token list [%u]:\n\n", ft_vector_get_size(vec_tokens));
-	i = 0;
-	while (i < ft_vector_get_size(vec_tokens))
+	(void)argc, (void)argv;
+	rl_catch_signals = false;
+	register_signal_handlers();
+	while (true)
 	{
-		ft_printf(
-			":%d %d \"%s\"[%d] { fd: %d, expandable: %d }\n",
-			vec_tokens[i].position,
-			vec_tokens[i].type,
-			vec_tokens[i].value,
-			vec_tokens[i].length,
-			vec_tokens[i].fd,
-			vec_tokens[i].is_expandable
-			);
-		i++;
-	}
-	ft_printf("\nEnd.\n\n");
-	t_ast ast;
-	t_parser parser = parser_create(&ast);
-	if (parser_parse(&parser, vec_tokens) != PARSER_OK)
-	{
-		ft_dprintf(STDERR_FILENO, "%S\n", create_parser_error_message(&parser.error_info));
-	}
-	else
-	{
-		ft_printf("Pipeline:\n");
-		for (size_t i = 0; i < ft_vector_get_size(ast.pipeline.vec_commands); i++)
+		line = readline("msh> ");
+		if (line == NULL)
 		{
-			t_ast_command command = ast.pipeline.vec_commands[i];
-			ft_printf("\tCommand:");
-			for (size_t i = 0; i < ft_vector_get_size(command.vec_argv); i++)
-				ft_printf(" %s", command.vec_argv[i]);
-			ft_printf("\n");
-			for (size_t i = 0; i < ft_vector_get_size(command.vec_redirections); i++)
+			free(line);
+			break ;
+		}
+		if (ft_strisempty(line))
+		{
+			rl_on_new_line();
+			continue ;
+		}
+		if (parse_command(line, &ast) == COMMAND_OK)
+		{
+			expand_executables(&ast);
+			for (size_t i = 0; i < ft_vector_get_size(ast.pipeline.vec_commands); i++)
 			{
-				ft_printf("\t\tRedirection: fd=%d source='%s'\n", command.vec_redirections[i].fd, command.vec_redirections[i].source);
+				t_ast_command command = ast.pipeline.vec_commands[i];
+				for (size_t i = 0; i < ft_vector_get_size(command.vec_argv); i++)
+					ft_printf("%s ", command.vec_argv[i]);
+				ft_printf("\n");
+				for (size_t i = 0; i < ft_vector_get_size(command.vec_redirections); i++)
+					ft_printf("\t&%d '%s'\n", command.vec_redirections[i].fd, command.vec_redirections[i].file);
+			}
+			for (size_t i = 0; i < ft_vector_get_size(ast.pipeline.vec_commands); i++)
+			{
+				if (is_builtin(ast.pipeline.vec_commands[i].vec_argv[0]))
+				{
+					exec_builtin(ast.pipeline.vec_commands[i].vec_argv[0], ast.pipeline.vec_commands[i].vec_argv);
+					continue ;
+				}
+				g_repl_state.child_process_id = fork();
+				if (g_repl_state.child_process_id == 0)
+				{
+					execve(ast.pipeline.vec_commands[i].vec_argv[0], ast.pipeline.vec_commands[i].vec_argv, environ);
+				}
+				waitpid(g_repl_state.child_process_id, NULL, 0);
+				rl_on_new_line();
+				g_repl_state.child_process_id = -1;
 			}
 		}
+		else
+		{
+			ft_printf("Syntax error!\n");
+		}
+		add_history(line);
+		free(line);
+		ast_destroy(&ast);
 	}
-	parser_destroy(&parser);
-	for (size_t i = 0; i < ft_vector_get_size(vec_tokens); i++)
-		free(vec_tokens[i].value);
-	ft_vector_free(vec_tokens);
-	for (size_t i = 0; i < ft_vector_get_size(ast.pipeline.vec_commands); i++)
-	{
-		for (size_t j = 0; j < ft_vector_get_size(ast.pipeline.vec_commands[i].vec_argv); j++)
-			free(ast.pipeline.vec_commands[i].vec_argv[j]);
-		for (size_t j = 0; j < ft_vector_get_size(ast.pipeline.vec_commands[i].vec_redirections); j++)
-			free(ast.pipeline.vec_commands[i].vec_redirections[j].source);
-		ft_vector_free(ast.pipeline.vec_commands[i].vec_argv);
-		ft_vector_free(ast.pipeline.vec_commands[i].vec_redirections);
-	}
-	ft_vector_free(ast.pipeline.vec_commands);
 	ft_clearenv();
 }
