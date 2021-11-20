7dc14c41c39bfa2e7c0f9cd1101eb8d07d31f73f
Pavel M <pamishenko@mail.com>
2021-11-20 13:28:38 +0300

    update exit

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: ttanja <ttanja@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/19 06:47:24 by ttanja            #+#    #+#             */
-/*   Updated: 2021/11/20 08:40:04 by ttanja           ###   ########.fr       */
+/*   Updated: 2021/11/20 13:27:48 by ttanja           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,31 +31,20 @@ int	is_number(char *str)
 			return (-1);
 		i++;
 	}
-	result = ft_atoi(str, NULL);
+	if (str)
+		result = ft_atoi(str, NULL);
 	return(result);
 }
 
 int	builtin_exit(int argc, char **argv)
 {
-	char	*exit_save_param;
-	
-	exit_save_param = calloc(sizeof(char), 1024);
-	ft_strlcpy(exit_save_param, "?=0", 3);
 	if (argc == 1)
-	{
-		builtin_export(1, &exit_save_param);
-		free(exit_save_param);
 		exit(0);
-	}
-	ft_strlcat(exit_save_param, ft_itoa((unsigned char)is_number(argv[1])), 10);
-	builtin_export(1, &exit_save_param);
-	if (is_number(argv[1]) < 0)
+	if (argv[1] && is_number(argv[1]) < 0)
 	{
 		printf("exit: %s: numeric argument required\n", argv[1]);
-		free(exit_save_param);
 		exit(255);
 	}
-	free(exit_save_param);
-	exit (is_number(argv[1]));
+	exit ((char)is_number(argv[1]));
 	return (0);
 }
