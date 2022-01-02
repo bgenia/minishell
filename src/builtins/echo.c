4140a4412c798b53524b69a26c049a8ace0b64d0
Pavel M <pamishenko@mail.com>
2022-01-02 20:19:08 +0300

    echo update

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -6,7 +6,7 @@
 /*   By: ttanja <ttanja@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/02 19:06:40 by ttanja            #+#    #+#             */
-/*   Updated: 2022/01/02 20:14:38 by ttanja           ###   ########.fr       */
+/*   Updated: 2022/01/02 20:18:40 by ttanja           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 int	builtin_echo(int argc, char **argv)
 {
 	if (argc > 3 && ft_strcmp(argv[1], "-n"))
-		ft_printf("%s", argv[1]);
-	ft_printf("%s/n", argv[1]);
+		ft_printf("%s", argv[2]);
+	ft_printf("%s/n", argv[2]);
 	return (0);
 }
