95a8e4ff177338dea2013ea524303103151e27e5
Pavel M <pamishenko@mail.com>
2022-01-02 19:21:56 +0300

    update echo

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -6,7 +6,7 @@
 /*   By: ttanja <ttanja@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/02 19:06:40 by ttanja            #+#    #+#             */
-/*   Updated: 2022/01/02 19:18:11 by ttanja           ###   ########.fr       */
+/*   Updated: 2022/01/02 19:21:10 by ttanja           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,6 @@
 int	builtin_echo(int argc, char **argv)
 {
 	(void)argc;
-	ft_printf("%s\n", argv[1]);
+	ft_printf("%s/n", argv[1]);
 	return (0);
 }
