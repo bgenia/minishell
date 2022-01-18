df040c05822773012a1e48337466a68a8d2bd401
Pavel M <pamishenko@mail.com>
2022-01-18 14:39:41 +0300

    update heredoc

--- include/minishell/redirects/heredoc.h
+++ include/minishell/redirects/heredoc.h
@@ -6,7 +6,7 @@
 /*   By: ttanja <ttanja@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/31 14:22:07 by bgenia            #+#    #+#             */
-/*   Updated: 2021/11/01 06:09:59 by ttanja           ###   ########.fr       */
+/*   Updated: 2022/01/18 13:08:31 by ttanja           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,5 @@
 # define HEREDOC_H
 
 char	*read_heredoc(char *name);
-char	*get_name_heredoc(char *str);
 
 #endif
--- src/redirects/read_heredoc.c
+++ src/redirects/read_heredoc.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   read_heredoc.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bgenia <bgenia@student.21-school.ru>       +#+  +:+       +#+        */
+/*   By: ttanja <ttanja@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/01 04:36:03 by ttanja            #+#    #+#             */
-/*   Updated: 2021/11/14 17:40:30 by bgenia           ###   ########.fr       */
+/*   Updated: 2022/01/18 14:38:17 by ttanja           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,39 +18,23 @@
 
 #include <readline/readline.h>
 
-char	*get_name_heredoc(char *str)
-{
-	size_t	i;
-	size_t	j;
-	char	*name;
-
-	i = 1;
-	while(str[i] && str[i - 1] != '<' && str[i] != '<')
-		i++;
-	j = i;
-	while(str[j] && str[j] != '\n')
-		j++;
-	name = ft_calloc(sizeof(char), j - i + 1);
-	ft_strlcpy(name, &str[i + 2], j - i );
-	return(name);
-}
-
 char	*read_heredoc(char *name)
 {
 	char	*input;
 	char	*temp;
+	char	*join;
 
 	input = ft_calloc(1,1);
 	temp = readline("heredoc> ");
-	if (!ft_strcmp(temp, name))
-		return ("");
 	while (ft_strcmp(temp, name))
 	{
-		ft_strjoin(input, temp);
+		join = ft_strjoin(input, temp);
 		free(temp);
+		free(input);
+		input = ft_strjoin(join, "\n");
+		free(join);
 		temp = readline("heredoc> ");
 	}
-	ft_strjoin(input, temp);
 	free(temp);
 	return(input);
 }
