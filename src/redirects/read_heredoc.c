97a10138e60e2647201b80fe896d296257d417fc
bgenia <bgenia@student.21-school.ru>
2022-02-26 01:18:22 +0300

    Make read_heredoc write directly to a pipe instead of a temp file

--- src/redirects/read_heredoc.c
+++ src/redirects/read_heredoc.c
@@ -3,34 +3,38 @@
 /*                                                        :::      ::::::::   */
 /*   read_heredoc.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ttanja <ttanja@student.21-school.ru>       +#+  +:+       +#+        */
+/*   By: bgenia <bgenia@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/01 04:36:03 by ttanja            #+#    #+#             */
-/*   Updated: 2022/02/25 14:32:56 by ttanja           ###   ########.fr       */
+/*   Updated: 2022/02/25 21:16:36 by bgenia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include <readline/readline.h>
+
 #include <libft/string/string.h>
 #include <libft/memory/memory.h>
 
+#include <stdio.h>
+#include <readline/readline.h>
+
 int	read_heredoc(char *name)
 {
-	char	*temp;
-	int		fd;
+	char	*line;
+	int		_pipe[2];
 
-	fd = open("/tmp/heredoc_temp", O_RDWR | O_CREAT);
-	temp = readline("heredoc> ");
-	while (ft_strcmp(temp, name))
+	pipe(_pipe);
+	line = readline("heredoc> ");
+	while (ft_strcmp(line, name))
 	{
-		write(fd, temp, ft_strlen(temp));
-		write(fd, "\n", 1);
-		free(temp);
-		temp = readline("heredoc> ");
+		write(_pipe[1], line, ft_strlen(line));
+		write(_pipe[1], "\n", 1);
+		free(line);
+		line = readline("heredoc> ");
 	}
-	free(temp);
-	return(fd);
+	free(line);
+	close(_pipe[1]);
+	return (_pipe[0]);
 }
