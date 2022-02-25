c8d2dfc82270ad86ca3bca4b576e42308f1a595b
Pavel M <pamishenko@mail.com>
2022-02-25 14:34:44 +0300

    Heredoc returns fd temporary file

--- src/redirects/read_heredoc.c
+++ src/redirects/read_heredoc.c
@@ -6,35 +6,31 @@
 /*   By: ttanja <ttanja@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/01 04:36:03 by ttanja            #+#    #+#             */
-/*   Updated: 2022/01/18 14:38:17 by ttanja           ###   ########.fr       */
+/*   Updated: 2022/02/25 14:32:56 by ttanja           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
-#include <stdio.h>
-
+#include <unistd.h>
+#include <fcntl.h>
+#include <readline/readline.h>
 #include <libft/string/string.h>
 #include <libft/memory/memory.h>
 
-#include <readline/readline.h>
-
-char	*read_heredoc(char *name)
+int	read_heredoc(char *name)
 {
-	char	*input;
 	char	*temp;
-	char	*join;
+	int		fd;
 
-	input = ft_calloc(1,1);
+	fd = open("/tmp/heredoc_temp", O_RDWR | O_CREAT);
 	temp = readline("heredoc> ");
 	while (ft_strcmp(temp, name))
 	{
-		join = ft_strjoin(input, temp);
+		write(fd, temp, ft_strlen(temp));
+		write(fd, "\n", 1);
 		free(temp);
-		free(input);
-		input = ft_strjoin(join, "\n");
-		free(join);
 		temp = readline("heredoc> ");
 	}
 	free(temp);
-	return(input);
+	return(fd);
 }
