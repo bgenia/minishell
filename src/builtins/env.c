5f897ac75f007bd1fc1d8efeeebba8252b53abb2
Pavel M <pamishenko@mail.com>
2022-03-07 18:25:15 +0300

    env.c norminette ok

--- src/builtins/env.c
+++ src/builtins/env.c
@@ -6,23 +6,22 @@
 /*   By: ttanja <ttanja@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/02 20:20:07 by ttanja            #+#    #+#             */
-/*   Updated: 2022/03/07 15:40:56 by ttanja           ###   ########.fr       */
+/*   Updated: 2022/03/07 18:24:41 by ttanja           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <libft/io/printf.h>
 #include <libft/system/env.h>
 
-
 int	builtin_env(int argc, char **argv, int fd)
 {
 	int	i;
 
 	if (argc > 1)
-		{
-			ft_printf("env: %s: No such file or directory", argv[1]);
-			return (127);
-		}
+	{
+		ft_printf("env: %s: No such file or directory", argv[1]);
+		return (127);
+	}
 	i = 0;
 	(void)argc;
 	(void)argv;
