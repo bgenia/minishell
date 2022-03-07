c8257c07c532f9c7282828470d6c4215785de679
Pavel M <pamishenko@mail.com>
2022-03-07 18:23:57 +0300

    unset.c norm ok

--- src/builtins/unset.c
+++ src/builtins/unset.c
@@ -6,7 +6,7 @@
 /*   By: ttanja <ttanja@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/20 09:06:20 by ttanja            #+#    #+#             */
-/*   Updated: 2022/03/07 15:33:41 by ttanja           ###   ########.fr       */
+/*   Updated: 2022/03/07 18:22:58 by ttanja           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,10 @@ int	builtin_unset(int argc, char **argv)
 {
 	(void)argc;
 	if (check_valid(argv[1]))
-		{
-			ft_printf("%s: : not a valid identifier", argv[1]);
-			return (1);
-		}
+	{
+		ft_printf("%s: : not a valid identifier", argv[1]);
+		return (1);
+	}
 	ft_unsetenv(argv[1]);
 	return (0);
 }
