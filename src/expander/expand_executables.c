901b78ab1fbf885b20e8ec9b27508d92557b041d
bgenia <bgenia@student.21-school.ru>
2022-03-09 01:26:37 +0300

    Fix infinite loop in expand_executables

--- src/expander/expand_executables.c
+++ src/expander/expand_executables.c
@@ -6,7 +6,7 @@
 /*   By: bgenia <bgenia@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/14 19:18:52 by bgenia            #+#    #+#             */
-/*   Updated: 2022/03/08 02:59:33 by bgenia           ###   ########.fr       */
+/*   Updated: 2022/03/08 23:18:59 by bgenia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 
 #include <minishell/parser/ast.h>
 #include <minishell/expander/expander.h>
-#include <minishell/builtins.h>
+#include <minishell/builtins/builtin_utils.h>
 
 void	expand_executables(t_ast *ast)
 {
@@ -27,7 +27,7 @@ void	expand_executables(t_ast *ast)
 	i = 0;
 	while (i < ft_vector_get_size(ast->pipeline.vec_commands))
 	{
-		program = &ast->pipeline.vec_commands[i].vec_argv[0];
+		program = &ast->pipeline.vec_commands[i++].vec_argv[0];
 		if (is_builtin(*program))
 			continue ;
 		executable = get_executable(*program);
@@ -36,6 +36,5 @@ void	expand_executables(t_ast *ast)
 			free(*program);
 			*program = executable;
 		}
-		i++;
 	}
 }
