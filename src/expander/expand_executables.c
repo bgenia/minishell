88723b3c6627106455cf2fbb7ca23b59113150e6
bgenia <bgenia@student.21-school.ru>
2022-03-08 04:48:38 +0300

    Fix expand_executables expanding builtins

--- src/expander/expand_executables.c
+++ src/expander/expand_executables.c
@@ -6,7 +6,7 @@
 /*   By: bgenia <bgenia@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/14 19:18:52 by bgenia            #+#    #+#             */
-/*   Updated: 2021/11/14 19:22:36 by bgenia           ###   ########.fr       */
+/*   Updated: 2022/03/08 02:59:33 by bgenia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,25 @@
 
 #include <minishell/parser/ast.h>
 #include <minishell/expander/expander.h>
+#include <minishell/builtins.h>
 
 void	expand_executables(t_ast *ast)
 {
 	size_t	i;
+	char	**program;
 	char	*executable;
 
 	i = 0;
 	while (i < ft_vector_get_size(ast->pipeline.vec_commands))
 	{
-		executable = get_executable(ast->pipeline.vec_commands[i].vec_argv[0]);
+		program = &ast->pipeline.vec_commands[i].vec_argv[0];
+		if (is_builtin(*program))
+			continue ;
+		executable = get_executable(*program);
 		if (executable)
 		{
-			free(ast->pipeline.vec_commands[i].vec_argv[0]);
-			ast->pipeline.vec_commands[i].vec_argv[0] = executable;
+			free(*program);
+			*program = executable;
 		}
 		i++;
 	}
