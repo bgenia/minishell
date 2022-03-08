6b19bc9e5f8e3e30ce46feccd46aee60806d2b82
bgenia <bgenia@student.21-school.ru>
2022-03-09 01:28:54 +0300

    Fix register_signal_handlers

--- src/shell/signals/register_signal_handlers.c
+++ src/shell/signals/register_signal_handlers.c
@@ -6,14 +6,14 @@
 /*   By: bgenia <bgenia@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/14 18:24:37 by bgenia            #+#    #+#             */
-/*   Updated: 2021/11/14 18:54:49 by bgenia           ###   ########.fr       */
+/*   Updated: 2022/03/08 23:10:37 by bgenia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 #include <signal.h>
 
-#include <minishell/repl/signals.h>
+#include <minishell/shell/signals.h>
 
 void	register_signal_handlers(void)
 {
