489250b004899b880cf20b581d10ee40c91d6c05
bgenia <bgenia@student.21-school.ru>
2022-03-09 01:26:00 +0300

    Remove unused variable from validate_env_identifier

--- src/builtins/builtin_utils/validate_env_identifier.c
+++ src/builtins/builtin_utils/validate_env_identifier.c
@@ -6,7 +6,7 @@
 /*   By: bgenia <bgenia@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/06 23:03:29 by ttanja            #+#    #+#             */
-/*   Updated: 2022/03/08 04:32:09 by bgenia           ###   ########.fr       */
+/*   Updated: 2022/03/08 23:11:14 by bgenia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,10 @@ bool
 	validate_env_identifier(char *identifier)
 {
 	int	i;
-	int	check;
 
 	if (identifier[0] != '_' && !ft_isalpha(identifier[0]))
 		return (true);
 	i = 0;
-	check = 0;
 	while (identifier[++i])
 	{
 		if (ft_isalpha(identifier[i])
