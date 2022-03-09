4d5f3741b9970f0c964d018925e0513eba79191d
bgenia <bgenia@student.21-school.ru>
2022-03-09 03:08:07 +0300

    Rewrite builtin_export, make it behave like in bash 5

--- src/builtins/builtins/bulitin_export.c
+++ src/builtins/builtins/bulitin_export.c
@@ -6,61 +6,46 @@
 /*   By: bgenia <bgenia@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/14 19:36:55 by bgenia            #+#    #+#             */
-/*   Updated: 2022/03/09 02:52:02 by bgenia           ###   ########.fr       */
+/*   Updated: 2022/03/09 03:06:57 by bgenia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdbool.h>
 #include <unistd.h>
+
+#include <minishell/builtins/builtin_utils.h>
+
 #include <libft/string/string.h>
 #include <libft/memory/memory.h>
 #include <libft/system/env.h>
 #include <libft/io/printf.h>
-#include <minishell/builtins/builtin_utils.h>
+#include <libft/array.h>
 
-static void
-	_sort_string(char **cp_env, int count)
+static int
+	_string_comparator(char **a, char **b, void *arg, size_t element_size)
 {
-	int		i;
-	int		j;
-	char	*temp;
-
-	i = -1;
-	while (++i < count)
-	{
-		j = -1;
-		while (++j < count - 1)
-		{
-			if (ft_strcmp(cp_env[j], cp_env[j + 1]) > 0)
-			{
-				temp = cp_env[j];
-				cp_env[j] = cp_env[j + 1];
-				cp_env[j + 1] = temp;
-			}
-		}
-	}
+	(void)arg, (void)element_size;
+	return (ft_strcmp(*a, *b));
 }
 
 static void
 	_print_sort_env(void)
 {
-	int		count;
-	int		i;
-	char	**cp_env;
+	size_t	env_size;
+	char	**env_copy;
+	size_t	i;
 
-	count = 0;
-	i = -1;
-	while (environ[count])
-		count++;
-	cp_env = ft_calloc(sizeof(char **), count + 1);
-	while (++i < count)
-		cp_env[i] = environ[i];
-	cp_env[i] = NULL;
-	_sort_string(cp_env, count);
-	i = -1;
-	while (++i < count)
-		ft_printf("%s\n", cp_env[i]);
-	free(cp_env);
+	env_size = ft_arrlen(environ, sizeof(*environ));
+	env_copy = ft_calloc(env_size + 1, sizeof(*env_copy));
+	ft_arrcpy(env_copy, environ, sizeof(*environ), env_size);
+	ft_qsort(env_copy, sizeof(*env_copy), env_size, (t_qsort_comparator){
+		.arg = NULL,
+		.comaprator = (void *) _string_comparator
+	});
+	i = 0;
+	while (env_copy[i])
+		ft_printf("declare -x %s\n", env_copy[i++]);
+	free(env_copy);
 }
 
 int
