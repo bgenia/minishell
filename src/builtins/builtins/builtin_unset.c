751cd6eb796aef92cf11b528222046ca3a2f6b49
bgenia <bgenia@student.21-school.ru>
2022-03-09 10:44:42 +0300

    Fix unset

--- src/builtins/builtins/builtin_unset.c
+++ src/builtins/builtins/builtin_unset.c
@@ -6,7 +6,7 @@
 /*   By: bgenia <bgenia@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/20 09:06:20 by ttanja            #+#    #+#             */
-/*   Updated: 2022/03/09 07:34:30 by bgenia           ###   ########.fr       */
+/*   Updated: 2022/03/09 10:38:28 by bgenia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,25 @@
 int
 	builtin_unset(int argc, char **argv)
 {
-	(void)argc;
-	if (validate_env_identifier(argv[1]))
+	int	status;
+	int	i;
+
+	status = 0;
+	i = 1;
+	while (i < argc)
 	{
-		ft_dprintf(STDERR_FILENO, _TERM_F_RED
-			"minishell: %s: not a valid identifier" _TERM_RESET, argv[1]);
-		return (1);
+		if (!validate_env_identifier(argv[i]))
+		{
+			ft_dprintf(STDERR_FILENO, _TERM_F_RED
+				"minishell: unset: '%s': not a valid identifier\n" _TERM_RESET,
+				argv[i]);
+			status = 1;
+		}
+		else
+		{
+			ft_unsetenv(argv[i]);
+		}
+		i++;
 	}
-	ft_unsetenv(argv[1]);
-	return (0);
+	return (status);
 }
