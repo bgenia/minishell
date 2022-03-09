33b65953ee9f79d33ddf3d3225f488d754da8f04
bgenia <bgenia@student.21-school.ru>
2022-03-09 03:22:46 +0300

    Fix overwriting std fds in shell_execute_builtins

--- src/shell/execution/execution/shell_execute_builtin.c
+++ src/shell/execution/execution/shell_execute_builtin.c
@@ -6,9 +6,11 @@
 /*   By: bgenia <bgenia@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/08 20:41:20 by bgenia            #+#    #+#             */
-/*   Updated: 2022/03/08 22:48:34 by bgenia           ###   ########.fr       */
+/*   Updated: 2022/03/09 03:20:40 by bgenia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <unistd.h>
 
 #include <minishell/parser/ast.h>
 #include <minishell/shell/shell.h>
@@ -27,5 +29,7 @@ int
 	shell_apply_redirections(command, ctx);
 	status = exec_builtin(command->vec_argv[0], command->vec_argv);
 	shell_close_redirections(command);
+	dup2(shell_get_state()->stdin_backup, STDIN_FILENO);
+	dup2(shell_get_state()->stdout_backup, STDOUT_FILENO);
 	return (status);
 }
