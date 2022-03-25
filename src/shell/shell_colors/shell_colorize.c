a4baa08bf9775d37420fcbdc11a7c6b61bb5f2c0
bgenia <bgenia@student.21-school.ru>
2022-03-25 22:37:15 +0300

    Remove define trickery from shell_colors.h, Add shell_colorize instead

--- src/shell/shell_colors/shell_colorize.c
+++ src/shell/shell_colors/shell_colorize.c
@@ -1,21 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   shell_colors.h                                     :+:      :+:    :+:   */
+/*   shell_colorize.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bgenia <bgenia@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/09 07:18:58 by bgenia            #+#    #+#             */
-/*   Updated: 2022/03/25 21:36:17 by bgenia           ###   ########.fr       */
+/*   Created: 2022/03/25 21:29:24 by bgenia            #+#    #+#             */
+/*   Updated: 2022/03/25 21:32:13 by bgenia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SHELL_COLORS_H
-# define SHELL_COLORS_H
+#include <minishell/shell/shell.h>
 
-# include <libft/terminal/termdefs.h>
+#include <libft/system/env.h>
 
 char
-*shell_colorize(char *color_code);
-
-#endif
+	*shell_colorize(char *color_code)
+{
+	if (ft_getenv("MSH_USE_COLORS"))
+		return (color_code);
+	return ("");
+}
