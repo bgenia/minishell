9d98b8c6511cc26eb210a15b79f02af464a42a6e
bgenia <bgenia@student.21-school.ru>
2022-04-29 20:22:29 +0300

    Make detailed error messages appear only if MSH_USE_DETAILED_ERRORS is set

--- src/shell/shell_utils/print_lexer_error.c
+++ src/shell/shell_utils/print_lexer_error.c
@@ -6,7 +6,7 @@
 /*   By: bgenia <bgenia@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/07 23:40:03 by bgenia            #+#    #+#             */
-/*   Updated: 2022/04/28 17:11:32 by bgenia           ###   ########.fr       */
+/*   Updated: 2022/04/29 20:21:29 by bgenia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,23 @@
 #include <minishell/shell/shell_utils.h>
 
 #include <ft/io/printf.h>
+#include <ft/process/env.h>
 
 void
 	print_lexer_error(t_lexer *lexer)
 {
 	if (lexer->status != LEXER_ERROR_UNCLOSED_QUOTE)
 	{
-		ft_dprintf(STDERR_FILENO, "%sminishell: unexpected lexer error\n%s",
+		ft_dprintf(STDERR_FILENO, "%sminishell: lexical error\n%s",
 			shell_colorize(TERM_F_RED), shell_colorize(TERM_RESET));
 		return ;
 	}
-	ft_dprintf(STDERR_FILENO, "%sminishell: syntax error near unclosed quote\n>"
-		"%s %s\n  ", shell_colorize(TERM_F_RED), shell_colorize(TERM_RESET),
-		lexer->source);
+	ft_dprintf(STDERR_FILENO, "%sminishell: syntax error near unclosed quote"
+		"%s\n", shell_colorize(TERM_F_RED), shell_colorize(TERM_RESET));
+	if (!ft_getenv("MSH_USE_DETAILED_ERRORS"))
+		return ;
+	ft_dprintf(STDERR_FILENO, "%s>%s %s\n  ", shell_colorize(TERM_F_RED),
+		shell_colorize(TERM_RESET), lexer->source);
 	print_many(STDERR_FILENO, ' ', lexer->position);
 	ft_dprintf(STDERR_FILENO, "%s^--- expected %c\n%s",
 		shell_colorize(TERM_F_RED), lexer->error_info.expected_quote,
--- src/shell/shell_utils/print_parser_error.c
+++ src/shell/shell_utils/print_parser_error.c
@@ -6,7 +6,7 @@
 /*   By: bgenia <bgenia@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/07 23:42:10 by bgenia            #+#    #+#             */
-/*   Updated: 2022/04/29 16:11:07 by bgenia           ###   ########.fr       */
+/*   Updated: 2022/04/29 20:21:24 by bgenia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@
 #include <ft/io/printf.h>
 #include <ft/string/string.h>
 #include <ft/math.h>
+#include <ft/process/env.h>
 
 void
 	print_parser_error(t_parser *parser, char *source)
@@ -30,9 +31,12 @@ void
 		parser->error_info.got.position = ft_strlen(source);
 	}
 	ft_dprintf(STDERR_FILENO, "%sminishell: syntax error near unexpected token "
-		"'%s'\n> %s%s\n%s  ", shell_colorize(TERM_F_RED),
-		parser->error_info.got.value, shell_colorize(TERM_RESET), source,
-		shell_colorize(TERM_F_RED));
+		"'%s'%s\n", shell_colorize(TERM_F_RED), parser->error_info.got.value,
+		shell_colorize(TERM_RESET));
+	if (!ft_getenv("MSH_USE_DETAILED_ERRORS"))
+		return ;
+	ft_dprintf(STDERR_FILENO, "%s> %s%s\n%s  ", shell_colorize(TERM_F_RED),
+		shell_colorize(TERM_RESET), source, shell_colorize(TERM_F_RED));
 	print_many(STDERR_FILENO, ' ', parser->error_info.got.position);
 	print_many(STDERR_FILENO, '^', ft_maxu(parser->error_info.got.length, 1));
 	ft_dprintf(STDERR_FILENO, "--- expected %S\n%s",
