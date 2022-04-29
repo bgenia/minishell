7554fa8c33570783181e33ba1c9546907c135522
bgenia <bgenia@student.21-school.ru>
2022-04-29 16:11:57 +0300

    Fix ^ not being printed when error token legnth == 0

--- src/shell/shell_utils/print_parser_error.c
+++ src/shell/shell_utils/print_parser_error.c
@@ -6,7 +6,7 @@
 /*   By: bgenia <bgenia@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/07 23:42:10 by bgenia            #+#    #+#             */
-/*   Updated: 2022/04/28 17:11:32 by bgenia           ###   ########.fr       */
+/*   Updated: 2022/04/29 16:11:07 by bgenia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@ void
 		parser->error_info.got.value, shell_colorize(TERM_RESET), source,
 		shell_colorize(TERM_F_RED));
 	print_many(STDERR_FILENO, ' ', parser->error_info.got.position);
-	print_many(STDERR_FILENO, '^', ft_minu(parser->error_info.got.length, 1));
+	print_many(STDERR_FILENO, '^', ft_maxu(parser->error_info.got.length, 1));
 	ft_dprintf(STDERR_FILENO, "--- expected %S\n%s",
 		stringify_expected_tokens(parser->error_info.expected),
 		shell_colorize(TERM_RESET));
