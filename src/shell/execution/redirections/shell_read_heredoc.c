8a5d41ccb5bd53a3498c58d9dd1920c92c13e675
bgenia <bgenia@student.21-school.ru>
2022-04-30 10:55:35 +0300

    Make heredoc prompt display heredoc name

--- src/shell/execution/redirections/shell_read_heredoc.c
+++ src/shell/execution/redirections/shell_read_heredoc.c
@@ -6,7 +6,7 @@
 /*   By: bgenia <bgenia@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/01 04:36:03 by ttanja            #+#    #+#             */
-/*   Updated: 2022/04/28 17:11:32 by bgenia           ###   ########.fr       */
+/*   Updated: 2022/04/30 10:54:50 by bgenia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,21 +22,30 @@
 #include <ft/string/string.h>
 #include <ft/memory/memory.h>
 #include <ft/io/printf.h>
+#include <ft/string/format.h>
 
 #include <readline/readline.h>
+
+static char
+	*_create_heredoc_prompt(char *name)
+{
+	return (ft_aformat("\001" TERM_F_LIGHT_BLUE "\002" "doc(" "\001" TERM_RESET
+			"\002" "%s" "\001" TERM_F_LIGHT_BLUE "\002" ")> " "\001" TERM_RESET
+			"\002", name));
+}
 
 int
 	shell_read_heredoc(char *name)
 {
+	char	*prompt;
 	char	*line;
 	int		_pipe[2];
 
 	pipe(_pipe);
+	prompt = _create_heredoc_prompt(name);
 	while (true)
 	{
-		line = readline(
-				"\001" TERM_F_LIGHT_BLUE "\002" "doc> " "\001" TERM_RESET "\002"
-				);
+		line = readline(prompt);
 		if (!line)
 			ft_dprintf(STDERR_FILENO, "%sminishell: warning: here-document "
 				"delimited by end-of-file (wanted '%s')\n%s",
@@ -48,6 +57,7 @@ int
 		free(line);
 	}
 	free(line);
+	free(prompt);
 	close(_pipe[1]);
 	return (_pipe[0]);
 }
